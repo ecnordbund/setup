3fce26d454413187c1a1cacc8316461b50a7ed8f
EC-Nordbund <39582214+ecnordbund@users.noreply.github.com>
2018-07-04 20:50:18 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+cd ec-api
+yarn
+cd ..
+cd lazyFilter
+yarn
+cd ..
+cd serienBrief
+yarn
+cd ..
+cd TN-Liste-Generator
+yarn
+cd ..
+cd Personenvergleich
+yarn
+cd ..
+cd ec-verwaltungs-app
+yarn
+cd electron
+yarn
+cd ..
+cd ..
