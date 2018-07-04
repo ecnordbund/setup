65ce121c06b65f08d716598e3234bc82280a8da6
EC-Nordbund <39582214+ecnordbund@users.noreply.github.com>
2018-07-04 20:49:50 +0200

    Create clone.bat

---
+++ clone.bat
@@ -0,0 +1,6 @@
+git clone https://www.github.com/ecnordbund/ec-verwaltungs-app
+git clone https://www.github.com/ecnordbund/ec-api
+git clone https://www.github.com/ecnordbund/lazyFilter
+git clone https://www.github.com/ecnordbund/serienBrief
+git clone https://www.github.com/ecnordbund/TN-Liste-Generator 
+git clone https://www.github.com/ecnordbund/Personenvergleich 
